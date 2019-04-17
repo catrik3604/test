e506721365c7462327124588067c04fbb7adbcef
catrik3604 <catrik3604@gmail.com>
2019-04-17 16:35:25 +0200

    instr. added

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 #test file
+
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/catrik3604/test.git
+git push -u origin master
